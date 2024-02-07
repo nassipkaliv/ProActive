4156b49e0abbd2870b1d3909ab93ed489d79078b
nassipkaliv <nassipkaliv@gmail.com>
2024-02-08 01:26:28 +0600

    faq collapse

--- js/app.js
+++ js/app.js
@@ -54,9 +54,9 @@ const swiper = new Swiper('.example-Swiper', {
   },
 });
 
-function toggleIcon() {
-  var plusIcon = document.querySelector('.svg-plus');
-  var minusIcon = document.querySelector('.svg-minus');
+function toggleIcon1() {
+  var plusIcon = document.getElementById('plus-1');
+  var minusIcon = document.getElementById('minus-1');
   
   if (plusIcon.style.display === 'none') {
     plusIcon.style.display = 'block';
@@ -86,4 +86,97 @@ function toggleIcon2() {
   var faqBlock = document.getElementById('faq-block2');
   faqBlock.classList.toggle('open');
 }
+
+function toggleIcon3() {
+  var plusIcon = document.getElementById('plus-3');
+  var minusIcon = document.getElementById('minus-3');
+  
+  if (plusIcon.style.display === 'none') {
+    plusIcon.style.display = 'block';
+    minusIcon.style.display = 'none';
+  } else {
+    plusIcon.style.display = 'none';
+    minusIcon.style.display = 'block';
+  }
+  var faqBlock = document.getElementById('faq-block3');
+  faqBlock.classList.toggle('open');
+}
+
+function toggleIcon4() {
+  var plusIcon = document.getElementById('plus-4');
+  var minusIcon = document.getElementById('minus-4');
+  
+  if (plusIcon.style.display === 'none') {
+    plusIcon.style.display = 'block';
+    minusIcon.style.display = 'none';
+  } else {
+    plusIcon.style.display = 'none';
+    minusIcon.style.display = 'block';
+  }
+  var faqBlock = document.getElementById('faq-block4');
+  faqBlock.classList.toggle('open');
+}
+
+function toggleIcon5() {
+  var plusIcon = document.getElementById('plus-5');
+  var minusIcon = document.getElementById('minus-5');
+  
+  if (plusIcon.style.display === 'none') {
+    plusIcon.style.display = 'block';
+    minusIcon.style.display = 'none';
+  } else {
+    plusIcon.style.display = 'none';
+    minusIcon.style.display = 'block';
+  }
+  var faqBlock = document.getElementById('faq-block5');
+  faqBlock.classList.toggle('open');
+}
+
+function toggleIcon6() {
+  var plusIcon = document.getElementById('plus-6');
+  var minusIcon = document.getElementById('minus-6');
+  
+  if (plusIcon.style.display === 'none') {
+    plusIcon.style.display = 'block';
+    minusIcon.style.display = 'none';
+  } else {
+    plusIcon.style.display = 'none';
+    minusIcon.style.display = 'block';
+  }
+  var faqBlock = document.getElementById('faq-block6');
+  faqBlock.classList.toggle('open');
+}
+
+function toggleIcon7() {
+  var plusIcon = document.getElementById('plus-7');
+  var minusIcon = document.getElementById('minus-7');
+  
+  if (plusIcon.style.display === 'none') {
+    plusIcon.style.display = 'block';
+    minusIcon.style.display = 'none';
+  } else {
+    plusIcon.style.display = 'none';
+    minusIcon.style.display = 'block';
+  }
+  var faqBlock = document.getElementById('faq-block7');
+  faqBlock.classList.toggle('open');
+}
+
+function toggleIcon8() {
+  var plusIcon = document.getElementById('plus-8');
+  var minusIcon = document.getElementById('minus-8');
+  
+  if (plusIcon.style.display === 'none') {
+    plusIcon.style.display = 'block';
+    minusIcon.style.display = 'none';
+  } else {
+    plusIcon.style.display = 'none';
+    minusIcon.style.display = 'block';
+  }
+  var faqBlock = document.getElementById('faq-block8');
+  faqBlock.classList.toggle('open');
+}
+
+
+
 
