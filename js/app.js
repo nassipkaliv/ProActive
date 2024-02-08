cf27ac88b108db6f6128ab6c45956d3ddee8689b
nassipkaliv <nassipkaliv@gmail.com>
2024-02-08 12:45:40 +0600

    faq collapse done!

--- js/app.js
+++ js/app.js
@@ -54,6 +54,14 @@ const swiper = new Swiper('.example-Swiper', {
   },
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+  var moreFaqCollapse = document.getElementById('moreFaq');
+  moreFaqCollapse.addEventListener('shown.bs.collapse', function() {
+    var questionBtn = document.getElementById('moreFaqButton');
+    questionBtn.style.display = 'none';
+  });
+});
+
 function toggleIcon1() {
   var plusIcon = document.getElementById('plus-1');
   var minusIcon = document.getElementById('minus-1');
@@ -176,6 +184,136 @@ function toggleIcon8() {
   var faqBlock = document.getElementById('faq-block8');
   faqBlock.classList.toggle('open');
 }
+
+function toggleIcon9() {
+  var plusIcon = document.getElementById('plus-9');
+  var minusIcon = document.getElementById('minus-9');
+  
+  if (plusIcon.style.display === 'none') {
+    plusIcon.style.display = 'block';
+    minusIcon.style.display = 'none';
+  } else {
+    plusIcon.style.display = 'none';
+    minusIcon.style.display = 'block';
+  }
+  var faqBlock = document.getElementById('faq-block9');
+  faqBlock.classList.toggle('open');
+}
+
+function toggleIcon10() {
+  var plusIcon = document.getElementById('plus-10');
+  var minusIcon = document.getElementById('minus-10');
+  
+  if (plusIcon.style.display === 'none') {
+    plusIcon.style.display = 'block';
+    minusIcon.style.display = 'none';
+  } else {
+    plusIcon.style.display = 'none';
+    minusIcon.style.display = 'block';
+  }
+  var faqBlock = document.getElementById('faq-block10');
+  faqBlock.classList.toggle('open');
+}
+
+function toggleIcon11() {
+  var plusIcon = document.getElementById('plus-11');
+  var minusIcon = document.getElementById('minus-11');
+  
+  if (plusIcon.style.display === 'none') {
+    plusIcon.style.display = 'block';
+    minusIcon.style.display = 'none';
+  } else {
+    plusIcon.style.display = 'none';
+    minusIcon.style.display = 'block';
+  }
+  var faqBlock = document.getElementById('faq-block11');
+  faqBlock.classList.toggle('open');
+}
+
+function toggleIcon12() {
+  var plusIcon = document.getElementById('plus-12');
+  var minusIcon = document.getElementById('minus-12');
+  
+  if (plusIcon.style.display === 'none') {
+    plusIcon.style.display = 'block';
+    minusIcon.style.display = 'none';
+  } else {
+    plusIcon.style.display = 'none';
+    minusIcon.style.display = 'block';
+  }
+  var faqBlock = document.getElementById('faq-block12');
+  faqBlock.classList.toggle('open');
+}
+
+function toggleIcon13() {
+  var plusIcon = document.getElementById('plus-13');
+  var minusIcon = document.getElementById('minus-13');
+  
+  if (plusIcon.style.display === 'none') {
+    plusIcon.style.display = 'block';
+    minusIcon.style.display = 'none';
+  } else {
+    plusIcon.style.display = 'none';
+    minusIcon.style.display = 'block';
+  }
+  var faqBlock = document.getElementById('faq-block13');
+  faqBlock.classList.toggle('open');
+}
+
+function toggleIcon14() {
+  var plusIcon = document.getElementById('plus-14');
+  var minusIcon = document.getElementById('minus-14');
+  
+  if (plusIcon.style.display === 'none') {
+    plusIcon.style.display = 'block';
+    minusIcon.style.display = 'none';
+  } else {
+    plusIcon.style.display = 'none';
+    minusIcon.style.display = 'block';
+  }
+  var faqBlock = document.getElementById('faq-block14');
+  faqBlock.classList.toggle('open');
+}
+
+function toggleIcon15() {
+  var plusIcon = document.getElementById('plus-15');
+  var minusIcon = document.getElementById('minus-15');
+  
+  if (plusIcon.style.display === 'none') {
+    plusIcon.style.display = 'block';
+    minusIcon.style.display = 'none';
+  } else {
+    plusIcon.style.display = 'none';
+    minusIcon.style.display = 'block';
+  }
+  var faqBlock = document.getElementById('faq-block15');
+  faqBlock.classList.toggle('open');
+}
+
+function toggleIcon16() {
+  var plusIcon = document.getElementById('plus-16');
+  var minusIcon = document.getElementById('minus-16');
+  
+  if (plusIcon.style.display === 'none') {
+    plusIcon.style.display = 'block';
+    minusIcon.style.display = 'none';
+  } else {
+    plusIcon.style.display = 'none';
+    minusIcon.style.display = 'block';
+  }
+  var faqBlock = document.getElementById('faq-block16');
+  faqBlock.classList.toggle('open');
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
